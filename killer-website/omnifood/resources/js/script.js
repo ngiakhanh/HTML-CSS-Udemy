9e53ad2a2beb653fce84b70583722db00d7ea7f0
WhiteK <ngiakhanh96@gmail.com>
2020-05-12 18:10:53 +0700

    Finish lecture 65

---
+++ killer-website/omnifood/resources/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    /* Sticky navigation */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    })
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        });
+    })
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        });
+    })
+
+    /* Navigation scroll */
+
+    /* Animations on scroll */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+        } else {
+            $('.js--wp-1').removeClass('animate__animated animate__fadeIn');
+        }
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--section-steps').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+        } else {
+            $('.js--wp-2').removeClass('animate__animated animate__fadeInUp');
+        }
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--section-cities').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+        } else {
+            $('.js--wp-3').removeClass('animate__animated animate__fadeIn');
+        }
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--section-plans').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('.js--wp-4').addClass('animate__animated animate__pulse');
+        } else {
+            $('.js--wp-4').removeClass('animate__animated animate__pulse');
+        }
+    }, {
+        offset: '50%'
+    })
+})
